c9e06de6173afff61557d9dda92943f99e07f242
Thong LT <letrthong@gmail.com>
2020-08-10 08:08:54 +0700

    clean up

--- 03.Iterators/iterators.py
+++ 03.Iterators/iterators.py
@@ -3,12 +3,9 @@ iterable_obj = iter(text)
   
 while True: 
     try: 
-  
-        # Iterate by calling next 
         item = next(iterable_obj) 
         print(item) 
     except StopIteration: 
-        # exception will happen when iteration will over 
         break
 
 print("") 
