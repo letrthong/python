dfa704dce964851af9e123c7a85bac7c43d6a15f
Thong LT <letrthong@gmail.com>
2020-08-10 08:17:30 +0700

    iterators

--- 03.Iterators/iterators.py
+++ 03.Iterators/iterators.py
@@ -8,6 +8,27 @@ while True:
     except StopIteration: 
         break
 
+print("")
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+while True: 
+    try: 
+        item = next(myit) 
+        print(item) 
+    except StopIteration: 
+        break
+
+print("") 
+cars = ["Ford", "Volvo"]
+sizeOfCar=len(cars)
+
+carIt = iter(cars)
+for i in range(sizeOfCar): 
+    item = next(carIt) 
+    print(item) 
+
+
 print("") 
 size= len(text)   
 for i in range(size): 
