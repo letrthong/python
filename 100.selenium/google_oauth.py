c8619b6ea06880cef4d9dd944c0dece173e24cda
Thong LT <letrthong@gmail.com>
2021-11-14 23:11:16 +0700

    Gooogle gmail

--- 100.selenium/google_oauth.py
+++
@@ -1,8 +0,0 @@
-# google-auth-oauthlib https://pypi.org/project/google-auth-oauthlib/
-# google-api-python-client https://pypi.org/project/google-api-python-client/
-# https://github.com/googleapis/google-api-python-client
-# https://google-auth-oauthlib.readthedocs.io/en/latest/reference/google_auth_oauthlib.html
-import sys
-
-from oauth2client import client
-from googleapiclient import sample_tools
