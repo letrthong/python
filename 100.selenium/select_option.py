4fbc6b11a9bf1b35520224b4f328a07792e85d21
Thong LT <letrthong@gmail.com>
2021-11-14 15:53:48 +0700

    find and select option

---
+++ 100.selenium/select_option.py
@@ -0,0 +1,24 @@
+# selenium = "4.0.0" Window  10 -64 bit
+# https://selenium-python.readthedocs.io/navigating.html
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+from selenium.webdriver.support.select import Select
+
+ser = Service(r"browsers\chromedriver.exe")
+op = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=op)
+
+driver.maximize_window()
+driver.get("https://updater-neon-bot-dev-01.ngrok.io/")
+time.sleep(1)
+select = Select(driver.find_element(By.ID, "id_header_language"))
+select.select_by_index(2)
+ 
+time.sleep(3)
+
+driver.close()
+driver.quit()
