9ebfc2910aef12106ee1457249132557c5dd704a
Thong LT <letrthong@gmail.com>
2022-03-28 04:54:24 +0200

    test

--- rs232/socat/socat.py
+++ rs232/socat/socat.py
@@ -3,8 +3,10 @@ import serial
 s1 = serial.Serial('/dev/pts/0')
 s2 = serial.Serial('/dev/pts/1')
 
-s1.write('hello\r\n')
+print("s1 sent")
+s1.write('hello socat\r\n')
 
+print("s2 recieve")
 data = s2.readline()
 print(data)
 
