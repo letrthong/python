0e5445d4a62c7d0216f8c9ec5b6e375b45f1cf9d
Thong LT <letrthong@gmail.com>
2022-03-31 11:21:47 +0700

    Create bytes.py

---
+++ rs232/bytes.py
@@ -0,0 +1,7 @@
+#https://www.geeksforgeeks.org/python-bytearray-function/
+import sys
+import time
+from datetime import datetime
+
+raw_factory = bytearray(id_factory,'utf-8')
+final = bytearray(128)
