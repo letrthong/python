e0a5d2cb9410f5eba680001d131436377c0265d3
Thong LT <letrthong@gmail.com>
2022-06-07 16:53:11 +0200

    thread asyncio

---
+++ setInterval/set_interval.py
@@ -0,0 +1,18 @@
+# python3  set_interval.py
+
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    await say_after(1, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(main())
