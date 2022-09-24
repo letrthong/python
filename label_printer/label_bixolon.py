81130552d283a849946685fc38f173850e113b9e
Thong LT <letrthong@gmail.com>
2022-09-24 21:54:45 +0700

    Update label_bixolon.py

--- label_printer/label_bixolon.py
+++ label_printer/label_bixolon.py
@@ -1,6 +1,9 @@
  
-from escpos import *
- 
+from escpos.printer  import Usb
+
+import usb.core
+import sys
+
 # BIXOLON SLP-TX403 - BPL-Z
 # lsusb
 # Bus 001 Device 002: ID 1504:0027 BIXOLON BIXOLON SLP-TX403
@@ -15,10 +18,18 @@ in_ep=0x81
 out_ep=0x02
 
 
-epson  = printer.Usb(idVendor, idProduct, interface , in_ep, out_ep)
 
-epson.text("Thonng Le Trung \n")
+device = usb.core.find(idVendor=idVendor, idProduct=idProduct)
+if device is None:
+    print('ADU Device not found. Please ensure it is connected to the tablet.')
+
+    
+    sys.exit(1)
+
+
+epson  = Usb(idVendor, idProduct, interface , in_ep, out_ep)
+epson.text("Telua Company \n")
+epson.qr("You can readme from your smartphone")
  
-#epson.barcode('1324354657687', 'EAN13', 64, 2, '', '')
 epson.cut()
  
