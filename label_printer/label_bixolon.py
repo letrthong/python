6fbed2f393fc4aee8fcd278d4b6730fe7289c998
Thong LT <letrthong@gmail.com>
2022-09-24 22:27:08 +0700

    Update label_bixolon.py

--- label_printer/label_bixolon.py
+++ label_printer/label_bixolon.py
@@ -6,7 +6,8 @@ import sys
  
 import os, sys 
 
-import win32print
+from win32printing import Printer
+ 
 
 # BIXOLON SLP-TX403 - BPL-Z
 # lsusb
@@ -25,18 +26,19 @@ out_ep=0x02
 
 device = usb.core.find(idVendor=idVendor, idProduct=idProduct)
 if device is None:
+
     print('ADU Device not found. Please ensure it is connected to the tablet.')
+    print('win32printing on Windows OS')
+    printer_name='BIXOLON SLP-TX403 - BPL-Z'
+
     font = {
         "height": 8,
     }
+ 
+    with Printer(linegap=1, printer_name=printer_name) as printer:
+        printer.text("title1", font_config=font)
 
-    p = win32print.OpenPrinter ("BIXOLON SLP-TX403 - BPL-Z")
-
-    job = win32print.StartDocPrinter (p, 1, ("test of raw data", None, "RAW")) 
-    win32print.StartPagePrinter (p) 
-    win32print.WritePrinter (p, "data to print")
-    win32print.EndPagePrinter (p)
-
+    
     sys.exit(1)
 
 
