9ed5f1e8c71a23dfa57f0fb8b82027162d333354
Thong LT <letrthong@gmail.com>
2023-04-04 07:37:52 +0200

    scan ip

---
+++ ip/country.py
@@ -0,0 +1,38 @@
+# 
+#  python3  country.py
+# 
+#  https://lite.ip2location.com/viet-nam-ip-address-ranges?lang=en_US 
+#
+# VN 1.52.0.0'  1.55.255.255 : Running
+#
+#
+
+import netaddr
+import requests
+import time
+import os.path
+
+start= int(netaddr.IPAddress('1.52.0.0'))
+print(start)
+end = int(netaddr.IPAddress('1.55.255.255'))
+
+path = "current_public_ip.txt"
+check_file = os.path.isfile(path)
+if check_file == True:
+    f = open( path, "r")
+    start = int(netaddr.IPAddress( f.read()))   
+    f.close()
+
+while start < end:
+    public_ip = str(netaddr.IPAddress(start))
+    print(public_ip)
+    api_url = "https://telua.co//services/v1/location/ip2countryCode?ip=" + public_ip
+    response = requests.get(api_url)
+    print( response.json())
+    start = start + 1
+
+   
+    f = open(path, "w")
+    f.write(public_ip)
+    f.close()
+    time.sleep(3)
