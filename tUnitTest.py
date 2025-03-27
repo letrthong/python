84f95099667c217ab4f56d0b60a4e40ee8eb92b7
Thong LT <letrthong@gmail.com>
2025-03-27 18:36:17 +0700

    Update tUnitTest.py

--- tUnitTest.py
+++ tUnitTest.py
@@ -2,16 +2,17 @@ import unittest
 
 from t_json import tJsonData
 
-class TestMathOperations(unittest.TestCase):
-    
-    def test_addition(self):
-        self.assertEqual(1 + 1, 2)
-    
-    def test_subtraction(self):
-        data_path = './data.json'
-        item_path = './item.json'
+class TestTJson(unittest.TestCase):
+    data_path = './data.json'
+    item_path = './item.json'
 
-        tJson = tJsonData( data_path )
+    def test_delete(self):
+        tJson = tJsonData( self.data_path )
+        tJson.remove_file()
+        self.assertEqual(0, tJson.get_size())
+    
+    def test_add_one_item(self):
+        tJson = tJsonData( self.data_path )
         tJson.remove_file()
 
         self.assertEqual(0, tJson.get_size())
@@ -19,13 +20,42 @@ class TestMathOperations(unittest.TestCase):
         items = {
             'temp': '4'
         }
-        tJson.create_item(items, item_path)
-        tJson.add_item(item_path)
+        tJson.create_item(items, self.item_path)
+        tJson.add_item(self.item_path)
         self.assertEqual(1, tJson.get_size())
 
         tJson.remove_first_item()
         self.assertEqual(0, tJson.get_size())
+    
+    def test_add_two_items(self):
+        tJson = tJsonData( self.data_path )
+        tJson.remove_file()
 
+        self.assertEqual(0, tJson.get_size())
+        
+        items = {
+            'temp': '4'
+        }
+        tJson.create_item(items, self.item_path)
+        tJson.add_item(self.item_path)
+
+        items = {
+            'temp': '5'
+        }
+        tJson.create_item(items, self.item_path)
+        tJson.add_item(self.item_path)
+        self.assertEqual(2, tJson.get_size())
+
+# if __name__ == '__main__':
+#     unittest.main(verbosity=2)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestTJson('test_delete'))
+    suite.addTest(TestTJson('test_add_one_item'))
+    suite.addTest(TestTJson('test_add_two_items'))
+    return suite
 
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
