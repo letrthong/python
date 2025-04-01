756d68f87c522f996efbcf964335836436e1bb2a
Thong LT <letrthong@gmail.com>
2025-04-01 14:02:42 +0700

    Create thread_qu1eue.py

---
+++ thread_qu1eue.py
@@ -0,0 +1,27 @@
+import threading
+import queue
+import time
+
+q = queue.Queue()
+
+def worker():
+    while True:
+        item = q.get()
+        print(f'Working on {item}')
+        print(f'Finished {item}')
+        q.task_done()
+
+# Turn-on the worker thread.
+threading.Thread(target=worker, daemon=True).start()
+
+# Send thirty task requests to the worker.
+for item in range(30):
+    q.put(item)
+
+# Block until all tasks are done.
+#q.join()
+
+for i in range(10):
+    print("time.sleep(1)")
+    time.sleep(1)
+print('All work completed')
